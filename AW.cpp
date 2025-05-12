2d8c04908b13629ce1e5aca94dddac16dab34fc8
AbdulWasaybinHassaan <universityworkonly01@gmail.com>
2025-05-13 00:49:26 +0500

    Create AW.cpp

---
+++ AW.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  
+using namespace std;  
+int main() {  
+    cout << "Name: Abdul Wasay Bin Hassaan" << endl;  
+    cout << "Roll Number: 24-ST-035" << endl;  
+    cout << "Department: Software Engineering Technology" << endl;  
+    return 0;  
+}  
